6d13b8f7c1d19a8fb6e8849619df76c673829ad0
zoma <aaroncr-dkz@users.noreply.github.com>
2018-05-26 21:52:50 -0600

    Fixed some connection object errors

--- web/instantCharacter/library/connections.php
+++ web/instantCharacter/library/connections.php
@@ -33,11 +33,11 @@ function herokuConnect() {
        $dbName = ltrim($dbopts["path"],'/');
 
        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	   
+	   return $db;
     }
     catch (PDOException $ex) {
-       echo 'Error!: ' . $ex->getMessage();
-       die();
+       echo "Server Error";
 	}
 }
